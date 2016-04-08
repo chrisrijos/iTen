6e38bf9eadddaadae596ff0013ca8c0edcaf74d4
Albert LeDuc <administrator@booksforthehungry.com>
2016-04-08 15:26:53 -0500

    Add Add Add

---
+++ Conference App/Home.swift
@@ -0,0 +1,35 @@
+//
+//  Home.swift
+//  Conference App
+//
+//  Created by B4TH Administrator on 4/8/16.
+//  Copyright © 2016 Chrystech Systems. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.restorationIdentifier = "HomeScreen"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
