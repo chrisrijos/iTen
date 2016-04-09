67ef55419455877b863757cb76b6d2048e029d46
Greg Gruse <ggruse@icfi.com>
2016-04-09 10:07:45 -0500

    This Commit should allow for testing of the agenda portion of the application, did some UI tweaks to the agenda portion .

---
+++ Conference App/AgendaViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  Agenda1
+//
+//  Created by Felipe Neves Brito on 4/5/16.
+//  Copyright © 2016 Felipe Neves Brito. All rights reserved.
+//
+
+import UIKit
+
+class AgendaViewController: UITableViewController {
+
+    var agendaController:AgendaController = AgendaController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.estimatedRowHeight = 85.0
+        tableView.rowHeight = UITableViewAutomaticDimension     // Sets the table view's row height to automatic
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        if(section == 0){
+            return agendaController.getEventsCount()
+        }
+        
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+    
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! EventCell
+        let event = agendaController.getEventAt(indexPath.row)
+        
+        cell.setName(event.name)
+        cell.setStartTime(event.timeStart)
+        cell.setStopTime(event.timeStop)
+        cell.setDate(event.date)
+
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+        
+        let add = UITableViewRowAction(style: .Normal, title: "Add to MyIten") { action, index in
+           
+            print("Selected")
+            print(indexPath.row)
+
+        }
+        add.backgroundColor = UIColor.lightGrayColor()
+        
+        return [add]
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // the cells you would like the actions to appear needs to be editable
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        // you need to implement this method too or you can't swipe to display the actions
+    }
+    
+    
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let destinationViewController: EventViewController
+        = (storyboard?.instantiateViewControllerWithIdentifier("EventViewController") as? EventViewController)!
+        
+        let event = self.agendaController.getEventAt(indexPath.row)
+        destinationViewController.event = event
+        
+        self.navigationController?.pushViewController(destinationViewController, animated: true)
+    }
+ 
+    
+}
+
