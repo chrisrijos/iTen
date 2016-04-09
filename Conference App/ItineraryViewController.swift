e91c99ceba26cb2d46929cec48169ef1f3370bf5
Greg Gruse <ggruse@icfi.com>
2016-04-09 10:09:33 -0500

    Added my own files for the Itinerary, however currently uncalled by the main view controller

---
+++ Conference App/ItineraryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  IteneraryViewController.swift
+//  Conference App
+//
+//  Created by Greg Gruse on 4/9/16.
+//  Copyright © 2016 Chrystech Systems. All rights reserved.
+//
+
+import UIKit
+
+class ItineraryViewController: AgendaViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
