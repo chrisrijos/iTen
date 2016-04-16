52f0bd3a81eaa0908e63daf598b2195527f26009
user115597 <user115597@mac090.local>
2016-04-16 17:31:22 -0400

    Added SocialMediaViewController with some social media methods. Added SocialMedia storyboard but have not added storyboard reference.

---
+++ Conference App/SociaMediaViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SociaMedia.swift
+//  Conference App
+//
+//  Created by user115597 on 4/16/16.
+//  Copyright © 2016 Chrystech Systems. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class SociaMediaViewController: UIViewController {
+    
+    @IBOutlet weak var facebook: UIButton!
+    @IBOutlet weak var twitter: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func postToFacebookTapped(sender: UIButton) {
+        post(toService: SLServiceTypeFacebook)
+    }
+    
+    @IBAction func postToTwitterTapped(sender: UIButton) {
+        post(toService: SLServiceTypeTwitter)
+    }
+    func post(toService service: String) {
+        let socialController = SLComposeViewController(forServiceType: service)
+        //            socialController.setInitialText("Hello World!")
+        //            socialController.addImage(someUIImageInstance)
+        //            socialController.addURL(someNSURLInstance)
+        self.presentViewController(socialController, animated: true, completion: nil)
+    }
+
+}
