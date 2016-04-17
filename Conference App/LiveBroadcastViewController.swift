a675833a792bab20a75d3ba0ec2b0052ea4b053c
Julian L <jlspecializd@macbook.local>
2016-04-17 10:56:53 -0500

    Changes to Live Broadcast

--- Conference App/LiveBroadcastViewController.swift
+++ Conference App/LiveBroadcastViewController.swift
@@ -25,27 +25,31 @@ class LiveBroadcastViewController: UIViewController {
     @IBOutlet var minutes: UILabel!
     @IBOutlet var seconds: UILabel!
     
+    // Starts the Audio Stream
     @IBAction func playAudio(sender: AnyObject) {
-//        if ViewController.isConnectedToNetwork() {
-//            print("Yes")
-//        }
-//        else {
-//            print("No")
-//        }
-        configureView()
-    } 
-    @IBAction func pauseAudio(sender: AnyObject) {
-        player!.pause()
-        player = nil
-        timer.invalidate()
+        startAudio()
     }
-    //@IBOutlet var audioView: UIWebView!
     
+    // Pauses the Stream
+    @IBAction func stopAudio(sender: AnyObject) {
+        if let tempPlayer = player {
+            tempPlayer.pause()
+            player = nil
+            count = 0
+            hours.text = "00"
+            minutes.text = "00"
+            seconds.text = "00"
+            timer.invalidate()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initial VC Styling
         self.view.backgroundColor = UIColor(red: 0.15, green: 0.353, blue: 0.6, alpha: 100)
         self.broadcastTitle.textColor = UIColor(red: 1, green: 0.63, blue: 0, alpha: 100)
+        
+        // This Code Lets the App Play MP3 in Background, Not Implemented Yet
         let session:AVAudioSession = AVAudioSession.sharedInstance()
         
         do {
@@ -53,11 +57,11 @@ class LiveBroadcastViewController: UIViewController {
         } catch {
             //catching the error.
         }
-        
-        
-        
     }
+    
+    // Figures out the duration that the stream has been going
     func audioDuration() {
+        
         count = count + 1
         let time = secondsToHoursMinutesSeconds(count)
         
@@ -67,35 +71,27 @@ class LiveBroadcastViewController: UIViewController {
         
         //print("Hours: \(String(format: "%02d", time.0))  Minutes: \(String(format: "%02d", time.1))  Seconds: \(String(format: "%02d", time.2))")
     }
-    func configureView() {
+    func startAudio() {
         var newtimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("audioDuration"), userInfo: nil, repeats: true)
         timer = newtimer
-        
         let url = "http://199.180.72.2:9110/;stream.mp3"
         let playerItem = AVPlayerItem( URL:NSURL( string:url )! )
+        
         player = AVPlayer(playerItem:playerItem)
-        player!.rate = 1.0;
-        player!.play()
+        
+        if let tempPlayer = player {
+            tempPlayer.rate = 1.0;
+            tempPlayer.play()
+        }
         
     }
     func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
         return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
     }
-//    class func isConnectedToNetwork() -> Bool {
-//        var zeroAddress = sockaddr_in()
-//        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
-//        zeroAddress.sin_family = sa_family_t(AF_INET)
-//        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-//            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
-//        }
-//        var flags = SCNetworkReachabilityFlags()
-//        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
-//            return false
-//        }
-//        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
-//        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-//        return (isReachable && !needsConnection)
-//    }
+    override func viewWillDisappear(animated: Bool) {
+        // Restart timer when user leaves the application
+        count = 0
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
